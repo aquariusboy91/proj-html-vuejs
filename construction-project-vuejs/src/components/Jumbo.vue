0716b4987bc29b8ce3ece5ac1b136bcb73a3cfea
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-24 13:22:30 +0100

    add fontawesome

--- construction-project-vuejs/src/components/Jumbo.vue
+++ construction-project-vuejs/src/components/Jumbo.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="container-fluid">
         <div clas="row">
-            
+            <i class="fas fa-folder text-white"></i>
         </div>
         
     </div>
 </template>
 
 <script>
+import '@fortawesome/fontawesome-free/css/all.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 export default {
     name: "Jumbo",
 
@@ -15,13 +19,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@fortawesome/fontawesome-free';
     .container-fluid {
-        background-image: url(../assets/img/work.jpg);
+        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../assets/img/work.jpg);
         height: 500px;
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
-        filter: brightness(45%);
+        background-image: brightness(60%);
     }
 
 </style>
