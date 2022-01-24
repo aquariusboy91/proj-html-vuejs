387822888041973bb6b62416a3267029151f33c6
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-24 11:20:41 +0100

    init & add navbar

---
+++ construction-project-vuejs/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+@import url('https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300&family=Roboto+Slab:wght@300&display=swap');
+
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
---
+++ construction-project-vuejs/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"> <img src="..\assets\img\construction_logo.png" alt="" class="logo"></a>
+    <div class="navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+     name: 'Header',
+};
+</script>
+
+<style lang="scss" scoped>
+    #navbarNavDropdown {
+        justify-content: flex-end;
+    }
+    * {
+        font-family: 'Libre Franklin', sans-serif;
+    }
+    .logo {
+        width: 210px;
+        height: 60px;
+
+    }
+
+</style>
