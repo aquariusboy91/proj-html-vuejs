96fcb92ecc35be16bc1df9913e07af1c8e961244
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-25 17:14:24 +0100

    add dinamic news

---
+++ construction-project-vuejs/src/components/CardsNews.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-cards">
+      <img :src="img">
+      <h3>{{titolo}}</h3>
+      <span>{{date}}</span>
+      <p>{{descr}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CardsNews',
+    props: [
+    'img',
+    'titolo',
+    'date',
+    'descr',
+    ]
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- construction-project-vuejs/src/components/MainNews.vue
+++ construction-project-vuejs/src/components/MainNews.vue
@@ -7,15 +7,48 @@
                  <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur voluptatibus exercitationem expedita aspernatur corporis, culpa deserunt nobis at unde ea cum quas laudantium illum sit maiores neque quis assumenda.</p>
             </div>
         </div>
+        <div class="cont-cards">
+                <CardsNews v-for="(card,index) in cards"
+                :key="index"
+                :titolo="card.title"
+                :date="card.date"
+                :descr="card.descr" />
+            </div>
     </div>
   
 </template>
 
 <script>
+import CardsNews from "./CardsNews.vue";
 export default {
     name: 'MainNews',
+    components: {
+     CardsNews,
+  },
+    data(){
+        return {
+            cards: [
+                {"img": '',
+                "title": 'Redeveloping Florida Remote Southern Coast', 
+                "date":'October 6th, 2021',
+                "descr": 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est'},
 
-}
+                {"img": '',
+                "title": 'How To Manage Large Construction Projects', 
+                "date":'January 31th, 2021',
+                "descr": 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est'},
+
+                {"img": '',
+                "title": 'Future Proofing A Modern Home', 
+                "date":'April 8th, 2022',
+                "descr": 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est'},
+                
+            ]
+        }
+    }
+
+    }
+
 </script>
 
 <style lang="scss" scoped>
