7d709ceeb3c26c24805f2aa6b717c475674d71cb
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-25 17:14:42 +0100

    add style cards

--- construction-project-vuejs/src/components/CardsNews.vue
+++ construction-project-vuejs/src/components/CardsNews.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="container-cards">
-      <img :src="img">
-      <h3>{{titolo}}</h3>
-      <span>{{date}}</span>
+  <div class="container-cards d-flex flex-column justify-content-center align-items-start">
+      <img :src="require(`../assets/img/${img}`)">
+      <h5>{{titolo}}</h5>
+      <p class="date">{{date}}</p>
+      <hr>
       <p>{{descr}}</p>
   </div>
 </template>
@@ -22,5 +23,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .container-cards {
+        width: 300px;
+        margin-right: 2em;
+        background-color: white;
+    }
+    img {
+        width: 300px;
+        height: 170px;
+    }
+    p {
+        font-size: 10px;
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+        color: gray;
+        line-height: 2em;
+    }
+    h5 {
+        margin-top: 1em;
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
+
+    .date {
+        font-size: 8px;
+        padding-left: 1em;
+        padding-right: 0.5em;
+        color: gray;
+    }
+    hr {
+        width: 80%;
+        margin: auto;
+        margin-bottom: 1em;
+        color: rgb(167, 167, 167)
+    }
+    
 
 </style>
--- construction-project-vuejs/src/components/MainNews.vue
+++ construction-project-vuejs/src/components/MainNews.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container-fluid mt-5">
+    <div class="container-fluid pt-5">
          <div class="row h-350 justify-content-center">
             <div class="col-8 text-center d-flex flex-column align-items-center">
                  <h3>Latest News</h3>
@@ -7,9 +7,10 @@
                  <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur voluptatibus exercitationem expedita aspernatur corporis, culpa deserunt nobis at unde ea cum quas laudantium illum sit maiores neque quis assumenda.</p>
             </div>
         </div>
-        <div class="cont-cards">
+        <div class="cont-cards d-flex justify-content-center">
                 <CardsNews v-for="(card,index) in cards"
                 :key="index"
+                :img="card.img"
                 :titolo="card.title"
                 :date="card.date"
                 :descr="card.descr" />
@@ -28,17 +29,17 @@ export default {
     data(){
         return {
             cards: [
-                {"img": '',
+                {"img": 'ruspaaa.jpg',
                 "title": 'Redeveloping Florida Remote Southern Coast', 
                 "date":'October 6th, 2021',
                 "descr": 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est'},
 
-                {"img": '',
+                {"img": 'bridge.jpg',
                 "title": 'How To Manage Large Construction Projects', 
                 "date":'January 31th, 2021',
                 "descr": 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est'},
 
-                {"img": '',
+                {"img": 'homemodern.jpg',
                 "title": 'Future Proofing A Modern Home', 
                 "date":'April 8th, 2022',
                 "descr": 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est'},
@@ -52,6 +53,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .container-fluid {
+        background-color:#f0ede6;
+        height: 870px;
+    }
     .p {
         color: grey;
         font-size: 12px;
